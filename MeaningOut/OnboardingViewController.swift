846e7ed920bb1c703c5fd3d86cc02282d04903f1
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-13 21:38:06 +0900

    [Fix] NoStoryBoard Setting

--- MeaningOut/OnboardingViewController.swift
+++ MeaningOut/OnboardingViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OnboardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .cyan
     }
 
 
