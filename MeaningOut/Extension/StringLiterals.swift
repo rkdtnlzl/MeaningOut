136fd5b648775645ac770465bdc2dd36e94af9fc
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-13 22:46:10 +0900

    Add] Enum 사용할 Literals 저장

---
+++ MeaningOut/Extension/StringLiterals.swift
@@ -0,0 +1,23 @@
+//
+//  StringLiterals.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 6/13/24.
+//
+
+import Foundation
+
+enum StringLiterals {
+    enum LabelText {
+        static let onboardingTitle = "MeaningOut"
+    }
+    
+    enum ButtonTitle {
+        static let launch = "시작하기"
+        static let finish = "완료"
+    }
+    
+    enum Placeholder {
+        static let requestNickName = "닉네임을 입력해주세요 :)"
+    }
+}
