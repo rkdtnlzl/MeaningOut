6fca645770bb80171540797c423146c5ba499e76
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-13 22:44:31 +0900

    [Add] Enum 사용할 Color Hex값 저장

---
+++ MeaningOut/Extension/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 6/13/24.
+//
+
+import UIKit
+
+enum Colors {
+    
+    static let orange = UIColor(hexCode: "EF8947")
+    static let gray = UIColor(hexCode: "828282")
+    static let darkGray = UIColor(hexCode: "4C4C4C")
+    static let lightGray = UIColor(hexCode: "CDCDCD")
+    
+    // 필요한 enum을 만들어서 사용해주세요
+    // 사용예시: titleLabel.text = StringLiterals.TabBar.home
+    
+}
