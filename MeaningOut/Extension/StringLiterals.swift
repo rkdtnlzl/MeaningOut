8133f72c9b7e5ce2b61b488303b6c35ad9180252
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-15 16:17:25 +0900

    [Add] StringLiterals 추가

--- MeaningOut/Extension/StringLiterals.swift
+++ MeaningOut/Extension/StringLiterals.swift
@@ -10,6 +10,13 @@ import Foundation
 enum StringLiterals {
     enum LabelText {
         static let onboardingTitle = "MeaningOut"
+        
+        enum NickNameStatus {
+            static let rightCase = "사용할 수 있는 닉네임입니다"
+            static let numberOfLiteralsCase = "2글자 이상 10글자 미만으로 설정해주세요"
+            static let specialLiteralsCase = "닉네임에 @, #, $, %는 포함할 수 없어요"
+            static let numberCase = "닉네임에 숫자는 포함할 수 없어요"
+        }
     }
     
     enum ButtonTitle {
@@ -19,5 +26,9 @@ enum StringLiterals {
     
     enum Placeholder {
         static let requestNickName = "닉네임을 입력해주세요 :)"
+    }
+    
+    enum NavigationTitle {
+        static let profileSetting = "PROFILE SETTING"
     }
 }
