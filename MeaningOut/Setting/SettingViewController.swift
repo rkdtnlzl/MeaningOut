0796681e4877850f4048c60f70398b7a92509b4b
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-17 18:22:03 +0900

    [Feat] 설정 tableview 구현

--- MeaningOut/Setting/SettingViewController.swift
+++ MeaningOut/Setting/SettingViewController.swift
@@ -6,13 +6,82 @@
 //
 
 import UIKit
+import SnapKit
 
-class SettingViewController: UIViewController {
-
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         
+        configureNavigation()
+        configureUI()
+        configureTableView()
     }
-
+    
+    func configureNavigation() {
+        navigationItem.title = "SETTING"
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        
+        if indexPath.row == 0 {
+            let nickname = UserDefaults.standard.string(forKey: "nickname") ?? ""
+            cell.textLabel?.text = "\(nickname)"
+            cell.textLabel?.font = .boldSystemFont(ofSize: 17)
+            cell.detailTextLabel?.text = "2024.06.15 가입"
+            cell.detailTextLabel?.textColor = .gray
+            cell.detailTextLabel?.font = .systemFont(ofSize: 14)
+            
+            let profileNumber = UserDefaults.standard.integer(forKey: "profileNumber")
+            cell.imageView?.image = UIImage(named: "profile_\(profileNumber)")
+            cell.imageView?.contentMode = .scaleAspectFill
+            cell.imageView?.clipsToBounds = true
+            cell.imageView?.layer.borderWidth = 5
+            cell.imageView?.layer.borderColor = UIColor.orange.cgColor
+            cell.imageView?.layer.cornerRadius = 33
+            cell.accessoryType = .disclosureIndicator
+        } else {
+            let titles = ["나의 장바구니 목록", "자주 묻는 질문", "1:1 문의", "알림 설정", "탈퇴하기"]
+            cell.textLabel?.text = titles[indexPath.row - 1]
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 120
+        } else {
+            return UITableView.automaticDimension
+        }
+    }
 }
