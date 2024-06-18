18c0b1081c3640e45feb0d3268e86e18a853854c
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-18 18:46:54 +0900

    [Feat] 프로필 수정 VC 기능 추가

---
+++ MeaningOut/Setting/ProfileImageModifyViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ProfileImageModifyViewController.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 6/18/24.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileImageModifyViewController: UIViewController, UICollectionViewDelegateFlowLayout {
+    
+    let profileImageView = UIImageView()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    let imageNames = (0...11).map { "profile_\($0)" }
+    var selectedIndexPath: IndexPath?
+    var selectedImage: UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureNavigation()
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        setTarget()
+    }
+    
+    func configureNavigation() {
+        navigationItem.title = StringLiterals.NavigationTitle.profileEdit
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(profileImageView)
+        view.addSubview(collectionView)
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.layer.cornerRadius = 50
+        profileImageView.layer.borderWidth = 5
+        profileImageView.layer.borderColor = Colors.orange.cgColor
+        profileImageView.clipsToBounds = true
+        let profileNumber = UserDefaults.standard.integer(forKey: "\(UserDefaultsKey.profileNumberKey)")
+        profileImageView.image = UIImage(named: "profile_\(profileNumber)")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        collectionView.backgroundColor = .white
+    }
+    
+    func configureLayout() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.width.height.equalTo(100)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+        }
+    }
+    
+    func setTarget() {
+        
+    }
+    
+    func layout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        layout.itemSize = CGSize(width: 80, height: 80)
+        return layout
+    }
+}
+
+
+extension ProfileImageModifyViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+        
+        let imageView = UIImageView(image: UIImage(named: imageNames[indexPath.item]))
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = cell.contentView.bounds
+        imageView.clipsToBounds = true
+        cell.contentView.addSubview(imageView)
+        cell.layer.cornerRadius = 40
+        cell.clipsToBounds = true
+        
+        if selectedIndexPath == indexPath {
+            cell.layer.borderColor = UIColor.orange.cgColor
+            cell.layer.borderWidth = 5
+            cell.contentView.alpha = 1.0
+        } else {
+            cell.layer.borderColor = UIColor.gray.cgColor
+            cell.layer.borderWidth = 1
+            cell.contentView.alpha = 0.5
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedIndexPath = indexPath
+        profileImageView.image = UIImage(named: imageNames[indexPath.item])
+        UserDefaults.standard.set(indexPath.item, forKey: "\(UserDefaultsKey.profileNumberKey)")
+        collectionView.reloadData()
+    }
+}
---
+++ MeaningOut/Setting/ProfileNicknameModifyViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ProfileNicknameModifyViewController.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 6/18/24.
+//
+
+import UIKit
+import SnapKit
+
+class ProfileNicknameModifyViewController: UIViewController {
+    
+    let profileImageView = UIImageView()
+    let profileImageButton = UIButton()
+    let nicknameTextField = UITextField()
+    let nicknameTextFieldLine = UIView()
+    let nicknameStatusLabel = UILabel()
+    var saveButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let profileNumber = UserDefaults.standard.integer(forKey: "\(UserDefaultsKey.profileNumberKey)")
+        profileImageView.image = UIImage(named: "profile_\(profileNumber)")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureNavigation()
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+        setTarget()
+    }
+    
+    func configureNavigation() {
+        navigationItem.title = StringLiterals.NavigationTitle.profileEdit
+        saveButton = UIBarButtonItem(title: "저장", style: .plain, target: self, action: #selector(saveButtonClicked))
+        saveButton.isEnabled = false
+        navigationItem.rightBarButtonItem = saveButton
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(profileImageView)
+        view.addSubview(profileImageButton)
+        view.addSubview(nicknameTextField)
+        view.addSubview(nicknameTextFieldLine)
+        view.addSubview(nicknameStatusLabel)
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        let profileNumber = UserDefaults.standard.integer(forKey: "\(UserDefaultsKey.profileNumberKey)")
+        profileImageView.image = UIImage(named: "profile_\(profileNumber)")
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.clipsToBounds = true
+        profileImageView.layer.cornerRadius = 50
+        profileImageView.layer.borderWidth = 5
+        profileImageView.layer.borderColor = Colors.orange.cgColor
+        
+        profileImageButton.setImage(UIImage(systemName: "camera.fill"), for: .normal)
+        profileImageButton.tintColor = .white
+        profileImageButton.backgroundColor = Colors.orange
+        profileImageButton.layer.cornerRadius = 16
+        
+        nicknameTextField.placeholder = StringLiterals.Placeholder.requestNickName
+        nicknameTextField.tintColor = .lightGray
+        
+        nicknameTextFieldLine.backgroundColor = .darkGray
+        
+        nicknameStatusLabel.text = ""
+        nicknameStatusLabel.textColor = Colors.orange
+        nicknameStatusLabel.font = .systemFont(ofSize: 13)
+    }
+    
+    func configureLayout() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.width.height.equalTo(100)
+        }
+        
+        profileImageButton.snp.makeConstraints { make in
+            make.bottom.trailing.equalTo(profileImageView)
+            make.width.height.equalTo(32)
+        }
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(30)
+            make.leading.trailing.equalTo(view.safeAreaInsets).inset(20)
+            make.height.equalTo(44)
+        }
+        
+        nicknameTextFieldLine.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaInsets).inset(20)
+            make.height.equalTo(1)
+        }
+        
+        nicknameStatusLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextFieldLine.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(view.safeAreaInsets).inset(20)
+        }
+    }
+    
+    func setTarget() {
+        nicknameTextField.addTarget(self, action: #selector(nicknameTextFieldDidChange), for: .editingChanged)
+        profileImageButton.addTarget(self, action: #selector(profileImageButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc func nicknameTextFieldDidChange(_ textField: UITextField) {
+        guard let text = textField.text else { return }
+        
+        let specialLiterals = CharacterSet(charactersIn: "@#$%")
+        let numbers = CharacterSet.decimalDigits
+        
+        if text.count < 2 || text.count > 9 {
+            nicknameStatusLabel.text = StringLiterals.LabelText.NickNameStatus.numberCase
+            saveButton.isEnabled = false
+            saveButton.tintColor = .gray
+        } else if text.rangeOfCharacter(from: specialLiterals) != nil {
+            nicknameStatusLabel.text = StringLiterals.LabelText.NickNameStatus.specialLiteralsCase
+            saveButton.isEnabled = false
+            saveButton.tintColor = .gray
+        } else if text.rangeOfCharacter(from: numbers) != nil {
+            nicknameStatusLabel.text = StringLiterals.LabelText.NickNameStatus.numberOfLiteralsCase
+            saveButton.isEnabled = false
+            saveButton.tintColor = .gray
+        } else {
+            nicknameStatusLabel.text = StringLiterals.LabelText.NickNameStatus.rightCase
+            saveButton.isEnabled = true
+            saveButton.tintColor = Colors.orange
+        }
+    }
+    
+    @objc func profileImageButtonClicked() {
+        let vc = ProfileImageModifyViewController()
+        vc.selectedImage = profileImageView.image
+        vc.navigationItem.hidesBackButton = false
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func saveButtonClicked() {
+        UserDefaults.standard.set(nicknameTextField.text, forKey: "\(UserDefaultsKey.nicknameKey)")
+        UserDefaults.standard.set(true, forKey: "\(UserDefaultsKey.isUserKey)")
+        navigationController?.popViewController(animated: true)
+    }
+}
