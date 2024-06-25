355a65d136a6d0d5e098449b558f36b8ab01d0b1
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-25 23:34:55 +0900

    [Feat] Base 객체 추가

---
+++ MeaningOut/Global/Base/BaseViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BaseViewController.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 6/25/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+    func configureView() { }
+}
