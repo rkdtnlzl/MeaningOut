78a1ede5b18fdfdafcaf6ba43fab729820e7a06c
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-30 00:03:19 +0900

    [Feat] identifier Cell 식별자 지정 Extension 추가

---
+++ MeaningOut/Global/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 6/30/24.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
