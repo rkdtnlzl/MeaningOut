9cc2f7df72f2fc06cae9cbbd6623ca850b0fffe8
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-02 13:51:57 +0900

    [Fix/#3] Static Dispatch를 이용한 성능 최적화
    상속, 오버라이딩 될 필요가 없는 클래스에 final 키워드 붙이기

--- MeaningOut/Presentation/Onboarding/OnboardingViewController.swift
+++ MeaningOut/Presentation/Onboarding/OnboardingViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class OnboardingViewController: BaseViewController {
+final class OnboardingViewController: BaseViewController {
     let onboardingTitleLabel = UILabel()
     let onboardingTitleImageView = UIImageView()
     let startButton = UIButton()
