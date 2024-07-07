cf4c0cc8fe31c45d4e4cef04db14f77a0467ac99
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-07 22:10:31 +0900

    [Feat] 탭바 nav 추가

--- MeaningOut/TabBarViewController.swift
+++ MeaningOut/TabBarViewController.swift
@@ -20,10 +20,14 @@ class TabBarController: UITabBarController {
         let nav1 = UINavigationController(rootViewController: search)
         nav1.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 0)
         
-        let setting = SettingViewController()
-        let nav2 = UINavigationController(rootViewController: setting)
-        nav2.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "person"), tag: 1)
+        let favofites = FavoritesViewController()
+        let nav2 = UINavigationController(rootViewController: favofites)
+        nav2.tabBarItem = UITabBarItem(title: "즐겨찾기", image: UIImage(systemName: "heart.fill"), tag: 1)
         
-        setViewControllers([nav1,nav2], animated: true)
+        let setting = SettingViewController()
+        let nav3 = UINavigationController(rootViewController: setting)
+        nav3.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "person"), tag: 2)
+        
+        setViewControllers([nav1,nav2,nav3], animated: true)
     }
 }
