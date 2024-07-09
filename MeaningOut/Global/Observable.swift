f1bf6b9a8cc7802757755e4b158ff35aa9f8b8ba
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-09 22:01:24 +0900

    [Feat] Observable 객체 추가

---
+++ MeaningOut/Global/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  MeaningOut
+//
+//  Created by 강석호 on 7/9/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        self.closure = closure
+    }
+}
